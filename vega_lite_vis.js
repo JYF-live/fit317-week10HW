7a08889bd7f6c64844656af6aad5bd88f825d796
Julian Foote <jfoo0022@student.monash.edu>
2025-10-08 14:46:43 +1100

    updated index.html, stacked_area_chart.json and the .js file for the script

---
+++ vega_lite_vis.js
@@ -0,0 +1,11 @@
+var vg_1 = "vega_gust_map.json";
+vegaEmbed("#map_chart", vg_1).then(function(result) {
+
+
+}).catch(console.error);
+
+var vg_2 = "stacked_area_chart_vega.json";
+vegaEmbed("#stacked_chart", vg_2).then(function(result) {
+
+
+}).catch(console.error);
