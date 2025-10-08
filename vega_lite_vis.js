baf2dc0c2c91175a978a3000af37f990d057f542
Julian Foote <jfoo0022@student.monash.edu>
2025-10-08 15:11:15 +1100

    updated files to better fit on page

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,11 +1,23 @@
 var vg_1 = "vega_gust_map.json";
-vegaEmbed("#map_chart", vg_1).then(function(result) {
-
-
+vegaEmbed("#map_chart", vg_1, {
+    width: 400,
+    height: 300,
+    actions: false
+}).then(function(result) {
+    // Optional: resize chart when window resizes
+    window.addEventListener('resize', function() {
+        result.view.resize();
+    });
 }).catch(console.error);
 
 var vg_2 = "stacked_area_chart_vega.json";
-vegaEmbed("#stacked_chart", vg_2).then(function(result) {
-
-
+vegaEmbed("#stacked_chart", vg_2, {
+    width: 400,
+    height: 300,
+    actions: false
+}).then(function(result) {
+    // Optional: resize chart when window resizes
+    window.addEventListener('resize', function() {
+        result.view.resize();
+    });
 }).catch(console.error);
